8c81f5ef574df44dbd66dccfcc331973f4bf6ba6
Jun <jun.w4ng@gmail.com>
2021-12-14 00:23:42 -0300

    Impresion lista paciente en navegador

--- main.js
+++ main.js
@@ -4,42 +4,44 @@ const { v4: uuidv4 } = require('uuid')
 const moment = require('moment')
 const _ = require('lodash')
 const chalk = require('chalk')
+const  url  = require('url')
 
 let allAppointments = []
 
 http.createServer((req, res) => {
 
-    axios.get('https://randomuser.me/api/')
-        .then(({ data }) => {
+    url.parse(req.url, true).query
+    if (req.url.includes('/list')) {
 
-            const patientData = data.results[0].name; // Data     
+        axios.get('https://randomuser.me/api/')
+            .then(({ data }) => {
 
-            const name = patientData.first; //Patient name
-            const lastName = patientData.last; //Patient last name
-            const id = uuidv4().slice(0, 6); //Unique Id with uuid
-            const date = moment().format('MMMM Do YYYY, h:mm:ss a'); //Timestamp
+                const patientData = data.results[0].name; // Data     
 
-            const url = req.url
+                const name = patientData.first; //Patient name
+                const lastName = patientData.last; //Patient last name
+                const id = uuidv4().slice(0, 6); //Unique Id with uuid
+                const date = moment().format('MMMM Do YYYY, h:mm:ss a'); //Timestamp
 
-            allAppointments.push(`Nombre: ${name} - Apellido: ${lastName} - ID: ${id} - Timestamp: ${date}`);
+                allAppointments.push(`Nombre: ${name} - Apellido: ${lastName} - ID: ${id} - Timestamp: ${date}`);
 
-            const color = chalk.blue.bgWhite; //Blue color and white background
+                const color = chalk.blue.bgWhite; //Blue color and white background
 
-            _.forEach(allAppointments, (value, key) => {
+                let htmlList = ''
 
-                const patient = `${key}. ${value}`
-                console.log(color(patient))
+                _.forEach(allAppointments, (value, i) => {
+                    
+                    const patient = `${i + 1}. ${value}`
+                    console.log(color(patient))
 
-                if (url === '/list') {
-                    res.write(`<p>${patient}</p>`)
-                }
+                    htmlList += `<p>${patient}</p>`
+                });
+
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end(htmlList)
+            })
+            .catch((error) => {
+                console.log(error);
             });
-
-            res.end()
-
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
+    };
 }).listen(8080, () => console.log('Server ON'));
